c6f59cc1ca57337219375a121546e4baf51dc8cf
Keita Onabuta <keonabut@microsoft.com>
2022-08-05 05:19:57 +0000

    formatted

--- src/model/train.py
+++ src/model/train.py
@@ -66,7 +66,7 @@ def parse_args():
 
     # add arguments
     parser.add_argument("--training_data", dest="training_data", type=str)
-    parser.add_argument("--reg_rate", dest="reg_rate", type=float, default=0.02)
+    parser.add_argument("--reg_rate", dest="reg_rate", type=float, default=0.01)
 
     # parse args
     args = parser.parse_args()
